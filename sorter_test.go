3f24fd5c7fd7b2a5c164ef6d8751b6df40738542
Victor Farazdagi <simple.square@gmail.com>
2015-09-03 02:38:18 +0300

    Parallel sorter implemented

---
+++ sorter_test.go
@@ -0,0 +1,45 @@
+package bitonic_test
+
+import (
+	"github.com/farazdagi/bitonic"
+	"math/rand"
+	"runtime"
+	"testing"
+	"time"
+)
+
+const SAMPLE_SIZE = 1 << 16
+
+var lst []int = make([]int, SAMPLE_SIZE)
+
+func randInt(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func init() {
+	runtime.GOMAXPROCS(4)
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+func shuffle() {
+	for i := 0; i < SAMPLE_SIZE; i++ {
+		lst[i] = randInt(0, SAMPLE_SIZE)
+	}
+}
+
+func TestSorter(t *testing.T) {
+	shuffle()
+	bitonic.Sort(lst, bitonic.SORT_ASC)
+
+	prev := lst[0]
+	for _, i := range lst {
+		if prev > i {
+			t.Fatal("failed to confirm ASC sorting")
+		}
+	}
+}
+
+func BenchmarkSorter(b *testing.B) {
+	shuffle()
+	bitonic.Sort(lst, bitonic.SORT_ASC)
+}
